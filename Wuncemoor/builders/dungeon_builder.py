ffa41adb98ab0f4fab15a6ee7eafd235fee26b96
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2020-03-17 00:59:07 -0400

    Add files via upload

--- Wuncemoor/builders/dungeon_builder.py
+++ Wuncemoor/builders/dungeon_builder.py
@@ -15,7 +15,7 @@ class DungeonDirector:
         
     def get_dungeon(self):
         
-        dungeon = Dungeon('NAME_UNDEFINED', 'FLOORS_UNDEFINED', 'MAPS_UNDEFINED')
+        dungeon = Dungeon('NAME_UNDEFINED', 'FLOORS_UNDEFINED', 'MAPS_UNDEFINED', 'NODEPOWER_UNDEFINED')
         
         name = self.__builder.get_name()
         dungeon.set_name(name)
@@ -26,16 +26,21 @@ class DungeonDirector:
         maps = self.__builder.get_maps()
         dungeon.set_maps(maps)
         
+        np = self.__builder.get_np()
+        dungeon.set_node_power(np)
+        
         return dungeon
             
     
 class DungeonBuilder:
     
-    def __init__(self, name, floors, width, height):
-        self.name = name
+    def __init__(self, basename, subtype, floors, width, height, np):
+        self.basename = basename
         self.floors = floors
         self.width = width
         self.height = height
+        self.subtype = subtype
+        self.np = np
     
     def initialize_maps(self):
         constants = get_constants()
@@ -46,11 +51,21 @@ class DungeonBuilder:
         
         return map
         
+    @property    
+    def name(self):
+        if self.subtype is None:
+            return self.basename
+        else:
+            return self.subtype + '_' + self.basename
+    
     def get_name(self):
         return self.name
         
     def get_floors(self):
         return self.floors
+    
+    def get_np(self):
+        return self.np
     
     def get_maps(self):
     
@@ -66,7 +81,7 @@ class DungeonBuilder:
         #Fill in the maps except for stairs
         for map in maps:
         
-            map.fill_map(self.name, constants['max_rooms'], constants['room_min_size'], constants['room_max_size'], constants['map_width'], constants['map_height'])
+            map.fill_map(self.basename, self.subtype, self.np, constants['max_rooms'], constants['room_min_size'], constants['room_max_size'], constants['map_width'], constants['map_height'])
 
         current_floor = 0
             
@@ -95,13 +110,14 @@ class DungeonBuilder:
             elif current_floor == (self.floors - 1):
                 
                 #Deepest floor has no stairs to go deeper
-                entrance_stairs_component = Stairs('Stairs going up', '<', self.name, current_floor-1, maps[current_floor-1].entrance)
+                entrance_stairs_component = Stairs('Stairs going up', '<', self.name, current_floor-1, maps[current_floor-1].exit)
                 entrance_stairs = Entity(maps[current_floor].entrance[0], maps[current_floor].entrance[1], libtcod.white, render_order=RenderOrder.STAIRS, stairs=entrance_stairs_component)
                 maps[current_floor].map_entities.append(entrance_stairs)
                 current_floor += 1
-
+        
+        maps[-1].add_boss(self.basename, self.subtype, self.np)
         return maps
-            
 
+        
         
     
--- Wuncemoor/builders/mob_builder.py
+++ Wuncemoor/builders/mob_builder.py
@@ -6,6 +6,7 @@ from components.equipment import Equipment
 from components.inventory import Inventory
 from components.phylo import Phylo
 from components.ai import BasicMonster
+from map_objects.mobs import orc, troll, goblin, goblin_mage, goblin_rogue, goblin_shaman, mini_goblin, mini_goblin_mage, mini_goblin_rogue, mini_goblin_shaman, mega_goblin, mega_goblin_rogue, mega_goblin_mage, mega_goblin_shaman, mini_kobold, mini_kobold_harasser, mini_kobold_trickster, mini_kobold_zealot, kobold, kobold_harasser, kobold_trickster, kobold_zealot, mega_kobold, mega_kobold_harasser, mega_kobold_trickster, mega_kobold_zealot, mini_rat, rat, mega_rat, mini_bat, bat, mega_bat, mini_salamander, salamander, mega_salamander, mini_spider, spider, mega_spider, mini_snail, snail, mega_snail, mini_shrimp, shrimp, mega_shrimp, mini_raccoon, raccoon, mega_raccoon, mini_bear, bear, mega_bear
 
 class MobDirector:
     
@@ -15,37 +16,8 @@ class MobDirector:
         self.__builder = builder
         
     def get_combatant(self):
-        combatant = Combatant('NAME_UNDEFINED', 'IMAGE_UNDEFINED', Phylo(), Attributes(), Level(), Competence(Strength(), Instinct(), Coordination(), Vitality(), Arcana(), Improvisation(), Wisdom(), Finesse(), Charisma(), Devotion()), Equipment(), Inventory(26))
         
-        name = self.__builder.get_name()
-        combatant.set_name(name)
-        
-        image = self.__builder.get_image()
-        combatant.set_image(image)
-        
-        phylo = self.__builder.get_phylo()
-        combatant.set_phylo(phylo)
-        
-        attributes = self.__builder.get_attributes()
-        combatant.set_attributes(attributes)
-        
-        level = self.__builder.get_level()
-        combatant.set_level(level)
-        
-        competence = self.__builder.get_competence()
-        combatant.set_competence(competence)
-        
-        equipment = self.__builder.get_equipment()
-        combatant.set_equipment(equipment)
-        
-        inventory = self.__builder.get_inventory()
-        combatant.set_inventory(inventory)
-        
-        ai = self.__builder.get_ai()
-        combatant.set_ai(ai)
-        
-        xp = self.__builder.get_xp()
-        combatant.set_xp(xp)
+        combatant = self.__builder.get_combatant()
         
         
         
@@ -57,56 +29,59 @@ class MobBuilder:
             self.node_power = node_power
             self.mob = mob
             
-        def get_name(self):
-            return self.mob
-            
-        def get_image(self):
-            mobs = { 'goblin': 'g', 'orc': 'o', 'troll': 'T' }
-            image = mobs[self.mob]
-            return image
-            
-        def get_phylo(self):
-            mobs = { 'orc': Phylo('sentient','orcish','orc','regular','melee_dps'),
-            'goblin': Phylo('sentient','goblinoid','goblin','regular','melee_dps'),
-            'troll': Phylo('sentient','giant','troll','regular','melee_dps') }
-            
-        def get_attributes(self):
-            
-            mobs = { 'goblin': Attributes(2,2,2,2,2,2,2,2,2,2),
-            'orc': Attributes(5,0,0,7,10,10,10,10,10,10), 
-            'troll': Attributes(1,0,0,10,10,10,10,10,10,10)}
-            attributes = mobs[self.mob]
-            return attributes
-            
-        def get_level(self):
-            
-            level = Level()
-            return level
-            
-        def get_competence(self):
-            
-            competence = Competence(Strength(),Instinct(),Coordination(),Vitality(),Arcana(),Improvisation(),Wisdom(),Finesse(),Charisma(),Devotion())
-            
-            return competence
-        
-        def get_equipment(self):
-            
-            return Equipment()
-            
-        def get_inventory(self):
-            
-            return Inventory(26)
-            
-        def get_ai(self):
-        
-            return BasicMonster()
-            
-        def get_xp(self):
-            
-            dict = { 'goblin': 100, 'orc': 350, 'troll': 1000}
-            xp = dict[self.mob]
-            return xp
-            
+        def get_combatant(self):
+            mobs = { 'orc': orc(),
+            'troll': troll(),
+            'mini_goblin': mini_goblin(),
+            'goblin': goblin(),
+            'mega_goblin': mega_goblin(),
+            'mini_goblin_mage': mini_goblin_mage(),
+            'goblin_mage': goblin_mage(),
+            'mega_goblin_mage': mega_goblin_mage(),
+            'mini_goblin_rogue': mini_goblin_rogue(),
+            'goblin_rogue': goblin_rogue(),
+            'mega_goblin_rogue': mega_goblin_rogue(),
+            'mini_goblin_shaman': mini_goblin_shaman(),
+            'goblin_shaman': goblin_shaman(),
+            'mega_goblin_shaman': mega_goblin_shaman(),
+            'mini_kobold': mini_kobold(),
+            'kobold': kobold(),
+            'mega_kobold': mega_kobold(),
+            'mini_kobold_harasser': mini_kobold_harasser(),
+            'kobold_harasser': kobold_harasser(),
+            'mega_kobold_harasser': mega_kobold_harasser(),
+            'mini_kobold_trickster': mini_kobold_trickster(),
+            'kobold_trickster': kobold_trickster(),
+            'mega_kobold_trickster': mega_kobold_trickster(),
+            'mini_kobold_zealot': mini_kobold_zealot(),
+            'kobold_zealot': kobold_zealot(),
+            'mega_kobold_zealot': mega_kobold_zealot(),
+            'mini_rat': mini_rat(),
+            'rat': rat(),
+            'mega_rat': mega_rat(),
+            'mini_bat': mini_bat(),
+            'bat': bat(),
+            'mega_bat': mega_bat(),
+            'mini_salamander': mini_salamander(),
+            'salamander': salamander(),
+            'mega_salamander': mega_salamander(),
+            'mini_spider': mini_spider(),
+            'spider': spider(),
+            'mega_spider': mega_spider(),
+            'mini_snail': mini_snail(),
+            'snail': snail(),
+            'mega_snail': mega_snail(),
+            'mini_shrimp': mini_shrimp(),
+            'shrimp': shrimp(),
+            'mega_shrimp': mega_shrimp(),
+            'mini_raccoon': mini_raccoon(),
+            'raccoon': raccoon(),
+            'mega_raccoon': mega_raccoon(),
+            'mini_bear': mini_bear(),
+            'bear': bear(),
+            'mega_bear': mega_bear(),
+            }
+            return mobs[self.mob]            
             
         
       
