31f10350f32335ce8a9e4ee2b0e059deff71f84e
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2020-07-26 23:37:24 -0400

    added abstract_builder
    DungeonBuilder converted to abstract, from which Dangerous/Safe Builders inherit. Starting to switch floors back to maps.

---
+++ abstracts/abstract_builder.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+
+from map_objects.concrete_maps import DangerousMap, SafeMap
+
+
+class AbstractDungeonBuilder(ABC):
+
+    def __init__(self, basename, subtype, floors, width, height, np):
+        self.basename = basename
+        self.floors = floors
+        self.width = width
+        self.height = height
+        self.subtype = subtype
+        self.np = np
+
+    @abstractmethod
+    def initialize_map(self):
+        pass
+
+    @abstractmethod
+    def get_maps(self):
+        pass
+
+    @property
+    def name(self):
+        if self.subtype is None:
+            return self.basename
+        else:
+            return self.subtype + '_' + self.basename
+
+    def get_name(self):
+        return self.name
+
+    def get_floors(self):
+        return self.floors
+
+    def get_np(self):
+        return self.np
+
+
+class DangerousDungeonBuilder(AbstractDungeonBuilder):
+
+    def initialize_map(self):
+        map = DangerousMap(self.width, self.height, variant=self.basename)
+        map.initialize_tiles()
+        return map
+
+    @abstractmethod
+    def get_maps(self):
+        pass
+
+
+class SafeDungeonBuilder(AbstractDungeonBuilder):
+
+    def initialize_map(self):
+        map = SafeMap(self.width, self.height, variant=self.basename)
+        map.initialize_tiles()
+        return map
+
+    @abstractmethod
+    def get_maps(self):
+        pass
+
