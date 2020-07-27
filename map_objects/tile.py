975351ead7b189ce49602d22690d4e72e1081a8e
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2020-07-26 23:58:38 -0400

    updated tile init
    starting to work towards a mutable world

--- map_objects/tile.py
+++ map_objects/tile.py
@@ -1,3 +1,11 @@
+from ECS.__entity.floor import GrassTileFloor, DirtTileFloor
+from ECS.__entity.blocker import RockTileBlocker
+from config.constants import IMAGE_OPTIONS
+from random import randint
+
+from config.image_objects import LIGHT_GRASS, DARK_GRASS, LIGHT_DIRT, DARK_DIRT, LIGHT_ROCK_WALL, DARK_ROCK_WALL
+
+
 class Tile:
     """
     Map tile. It may or may not be blocked, and may or may not block sight.
@@ -6,14 +14,10 @@ class Tile:
     Mode changes tile appearance based on neighboring nodes. (Ex. Different road pieces)
     Node power increases risk/reward
     """
-    def __init__(self, blocked, block_sight=None):
-        self.blocked = blocked
-        
-        # By default if a tile is blocked, it also blocks sight.
-        if block_sight is None:
-            block_sight = blocked
-            
-        self.block_sight = block_sight
+    def __init__(self, variant):
+        self.base = self.initialize_base(variant)
+        self.blocker = self.initialize_blocker(variant)
+        self.block_sight = self.initialize_block_sight()
         self.explored = False
         self.type = None
         self.subtype = None
@@ -22,3 +26,31 @@ class Tile:
         self.image = None
         # Temporary dark image until fov shader implemented
         self.image2 = None
+        # Temp holder for images that appear overworld entities
+        self.image_air = None
+
+    def initialize_base(self, variant):
+        if variant == 'town':
+            base = GrassTileFloor()
+        elif variant == 'cave':
+            base = DirtTileFloor()
+        elif variant == 'overworld':
+            base = None
+        return base
+
+    def initialize_blocker(self, variant):
+        if variant in ('town', 'overworld'):
+            return None
+        if variant == 'cave':
+            return RockTileBlocker()
+
+    def initialize_block_sight(self):
+        if self.blocker is None:
+            return False
+        else:
+            return self.blocker.opaque
+
+    def clear_blocker(self):
+        self.blocker = None
+
+
