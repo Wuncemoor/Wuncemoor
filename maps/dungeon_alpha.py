42fe4485bc66db1bf0498560378fd064145d2063
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2020-07-27 22:25:20 -0400

    added DungeonAlphaMixin
    new mixin to help create starting town

---
+++ maps/dungeon_alpha.py
@@ -0,0 +1,17 @@
+from ECS.entity import Entity
+from ECS.__entity.age import Age
+from ECS.__entity.noncombatant import Noncombatant
+from config.image_objects import BUNDLE_SAMWISE
+from dialogue.get_dialogue import get_samwise_dialogue
+from enums.render_order import RenderOrder
+
+
+class DungeonAlphaMixin:
+
+    def get_samwise(self):
+
+        dialogue = get_samwise_dialogue()
+        noncom = Noncombatant('samwise', BUNDLE_SAMWISE, dialogue)
+        age = Age(13, 12, 28, 0, (1, 4))
+        samwise = Entity(18, 16, blocks=False, render_order=RenderOrder.ACTOR, noncombatant=noncom, age=age)
+        return samwise
