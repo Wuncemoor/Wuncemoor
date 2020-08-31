4a04b469a5aaca65c9a7b8fa21df91fdf0487941
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2020-08-31 04:39:07 -0400

    added documentation

--- handlers/views/camera.py
+++ handlers/views/camera.py
@@ -2,14 +2,19 @@ from config.constants import TILES_ON_SCREEN
 
 
 class Camera:
+    """Positioned in the top left corner of the game screen and follows the Party. Used by ArtistHandler to render
+    tiles relative to Camera coordinates. """
     
     def __init__(self):
         self.x = 0
         self.y = 0
 
     def refocus(self, px, py):
+        """Called when Party coordinates changes, most commonly through a successful MOVE action or using a Transition
+        to another Map or Dungeon. Fits to walls/corners as Party approaches."""
         (width, height) = TILES_ON_SCREEN
-        
+
+        # Ensures all tiles are valid to avoid bound errors/displaying "empty" tiles on potentially half of the screen
         if px < width/2:
             self.x = 0
         elif px > self.owner.owner.world.width - width/2:
