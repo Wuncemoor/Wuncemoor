1e4dd4d4412b2c3ffdfcb37745ac5a24fae8b38b
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2020-09-17 14:43:56 -0400

    Create inn_blockers.py
    new module for all the Tile blockers used by TownAlphaInn

---
+++ map_objects/blockers/inn_blockers.py
@@ -0,0 +1,49 @@
+from abstracts.abstract_tile_component import AbstractTileBlocker
+from config.image_objects import PILLAR_MID, PILLAR_TOP, PILLAR_BOT, WALL_MID, WALL_TOP_H, WALL_BOT, WALL_TOP_V
+
+
+class PillarTileBlocker(AbstractTileBlocker):
+
+    name = 'Pillar'
+    opaque = True
+    overshadow = [PILLAR_MID, PILLAR_TOP]
+
+    def __init__(self):
+        self.light_image = PILLAR_BOT
+        self.dark_image = PILLAR_BOT
+
+
+class HWallTileBlocker(AbstractTileBlocker):
+
+    name = 'Wall'
+    opaque = True
+    overshadow = [WALL_MID, WALL_TOP_H]
+
+    def __init__(self):
+        self.light_image = WALL_BOT
+        self.dark_image = WALL_BOT
+
+
+class VWallTileBlocker(AbstractTileBlocker):
+
+    name = 'Wall'
+    opaque = True
+    overshadow = None
+
+    def __init__(self):
+        self.light_image = WALL_TOP_V
+        self.dark_image = WALL_TOP_V
+
+
+def inn_blocker_array():
+    pil = PillarTileBlocker()
+    hw = HWallTileBlocker()
+    vw = VWallTileBlocker()
+    array = [[pil,  hw,   hw,   hw, None,   hw,   hw,  hw, pil],
+             [vw, None, None, None, None, None, None, None, vw],
+             [vw, None, None, None, None, None, None, None, vw],
+             [vw, None, None, None, None, None, None, None, vw],
+             [vw, None, None, None, None, None, None, None, vw],
+             [vw, None, None, None, None, None, None, None, vw],
+             [pil,  hw,   hw,   hw,   hw,   hw,   hw,  hw, pil]]
+    return array
