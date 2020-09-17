5466ee9935a5d18887c00ee1c0c6ac017867c4fb
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2020-09-17 14:48:46 -0400

    Improved DungeonAlphaMixin
    Mixin now has get_major_road/structures/rects, streamlining Dungeon building process

--- dungeons/dungeon_alpha.py
+++ dungeons/dungeon_alpha.py
@@ -4,15 +4,36 @@ from ECS.__entity.noncombatant import Noncombatant
 from config.image_objects import BUNDLE_SAMWISE
 from dialogue.get_dialogue import get_samwise_dialogue
 from enums.render_order import RenderOrder
+from map_objects.majorroad import MajorRoad
+from map_objects.rect import Rect
+from prefabs.alpha_prefabs import TownAlphaInn
 
 
 class DungeonAlphaMixin:
     """Mixin for DungeonAlphaBuilder, adds functionality unique to starting town"""
 
-    def get_samwise(self):
+    @staticmethod
+    def get_samwise():
 
         dialogue = get_samwise_dialogue()
         noncom = Noncombatant('samwise', BUNDLE_SAMWISE, dialogue)
         age = Age(13, 12, 28, 0, (1, 4))
         samwise = Entity(5, 16, blocks=False, render_order=RenderOrder.ACTOR, noncombatant=noncom, age=age)
         return samwise
+
+    def get_major_road(self):
+        rect = Rect(0, int(self.height / 2) - 2, self.width, 4)
+        road = MajorRoad(rect, self.node)
+        road.set_transitions('vertical')
+        return road
+
+    def get_structures(self):
+        structures = [TownAlphaInn]
+        return structures
+
+    def get_rects(self):
+        rects = [Rect(25, 25, 9, 7)]
+        return rects
+
+
+
