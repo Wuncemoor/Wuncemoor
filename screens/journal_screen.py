5e967e7a63711e9cd16ddc54afa809b377fd4a0e
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2020-12-09 23:59:54 -0500

    reduced journal size
    Journal is now same size as Character Sheet

--- screens/journal_screen.py
+++ screens/journal_screen.py
@@ -1,4 +1,4 @@
-from screens.gui_tools import get_surface, get_alpha_surface, align_and_blit, get_button_surface, get_text_surface, get_wrapped_text
+from screens.gui_tools import get_surface, get_alpha_surface, align_and_blit, get_text_surface, get_wrapped_text
 from config.image_objects import JOURNAL_OBJS
 from config.constants import RED, DARK_RED, BLACK, YELLOW_SELECT, GREY, WHITE
 
@@ -12,8 +12,9 @@ def journal_screen(self):
 
         j_display = journal_options_display(sj, self.game.options.current.choice)
         details = get_entry_details(sj[self.game.options.current.choice])
-        surf.blit(j_display, (83, 278))
-        surf.blit(details, (812, 270))
+        surf.blit(j_display, (int(surf.get_width()*0.05), int(surf.get_height()*0.25)))
+        surf.blit(details, (int(surf.get_width()*0.43), int(surf.get_height()*0.255)))
+
     else:
         ind = self.game.options.current.choice
 
@@ -24,13 +25,12 @@ def journal_screen(self):
     codex = JOURNAL_OBJS.get('codex')
     history = JOURNAL_OBJS.get('history')
 
-    surf.blit(text, (71, 218))
-
-    surf.blit(selected_icon, (825 + 75 * ind, 120))
-    surf.blit(current, (825, 120))
-    surf.blit(completed, (900, 120))
-    surf.blit(codex, (975, 120))
-    surf.blit(history, (1050, 120))
+    align_and_blit(surf, text, x_ratio=0.227, y_ratio=0.227)
+    align_and_blit(surf, selected_icon, x_ratio=0.426+(0.05*ind), y_ratio=0.131)
+    align_and_blit(surf, current, x_ratio=0.426, y_ratio=0.131)
+    align_and_blit(surf, completed, x_ratio=0.476, y_ratio=0.131)
+    align_and_blit(surf, codex, x_ratio=0.526, y_ratio=0.131)
+    align_and_blit(surf, history, x_ratio=0.576, y_ratio=0.131)
 
     align_and_blit(self.screen, surf)
 
@@ -46,33 +46,33 @@ def journal_options_display(subjournal, option):
         else:
             color = GREY
         bg = get_surface(obj)
-        text = get_text_surface(i.title, 30, color, 'source_sans_pro')
-        bg.blit(text, (80, 5))
+        text = get_text_surface(i.title, 18, color, 'source_sans_pro')
+        align_and_blit(bg, text)
         surf.blit(bg, (0, 0 + (y * obj.get_height())))
     return surf
 
 
 def get_entry_details(entry):
-    surf = get_alpha_surface(946, 765)
-    align_and_blit(surf, get_text_surface(entry.title, 36, BLACK), y_ratio=0.1)
-    y_off = 130
+    surf = get_alpha_surface(367, 400)
+    align_and_blit(surf, get_text_surface(entry.title, 22, BLACK), y_ratio=0.1)
+    y_off = 80
     for i in entry.plot:
         sc = get_story_chunk(i)
-        surf.blit(sc, (60, y_off))
+        surf.blit(sc, (30, y_off))
         y_off += sc.get_height() + 30
     return surf
 
 
 def get_story_chunk(chunk):
-    width = 946
-    titlesize = 28
-    fontsize = 22
+    width = 300
+    titlesize = 18
+    fontsize = 14
     lines = get_wrapped_text(chunk.info, width, fontsize, color=BLACK)
 
     surf = get_alpha_surface(width, titlesize + (fontsize * 1.2) * len(lines))
     surf.blit(get_text_surface(chunk.title, titlesize, WHITE), (0, 0))
 
-    y = 40
+    y = 20
     for line in lines:
         surf.blit(line, (0, y))
         y += fontsize
