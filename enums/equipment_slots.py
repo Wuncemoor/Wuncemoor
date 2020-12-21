b6133dc4075124545acec41260d693ee69d60312
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2020-12-21 02:21:32 -0500

    Update EquipmentSlots
    added more slots for proper adventuring

--- enums/equipment_slots.py
+++ enums/equipment_slots.py
@@ -3,14 +3,24 @@ from enum import Enum
 
 class EquipmentSlots(Enum):
     """All the places that a Combatant can put Items in their Equipment"""
-    MAIN_HAND = 1
-    OFF_HAND = 2
-    HEAD = 3
-    BODY = 4
-    FEET = 5
-    BELT = 6
-    HANDS = 7
-    FINGER = 8
+    HEAD = 1
+    SHOULDERS = 2
+    CHEST = 3
+    ARMS = 4
+    HANDS = 5
+    LEGS = 6
+    FEET = 7
+    FACE = 8
     NECK = 9
     BACK = 10
+    WAIST = 11
+    FINGER1 = 12
+    FINGER2 = 13
+    SATCHEL = 14
+    MAIN_HAND = 15
+    OFF_HAND = 16
+    QUIVER = 17
+    FOOD = 18
+    DRINK = 19
+
 
