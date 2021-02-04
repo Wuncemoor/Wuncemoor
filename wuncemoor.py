a5d311e019a0d4b1e1566f73d5e2e475067a1d11
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2021-02-04 04:03:08 -0500

    added engine folder
    Beginning to restructure code to separate engine logic from game data
    
    moved some boot code into engine\initialize
    
    Added TitleStates Enum

--- wuncemoor.py
+++ wuncemoor.py
@@ -1,30 +1,13 @@
 import pygame as py
-from config.constants import SCREEN_SIZE, CAPTION, KEYDOWN_DELAY
-from handlers.game_handler import GameHandler
-from handlers.artist_handler import ArtistHandler
-from handlers.input_handler import InputHandler
-from handlers.logic_handler import LogicHandler
-from handlers.options_handler import OptionsHandler
+import engine.initialize as initialize
 
 
 def main():
+    screen, clock = initialize.pygame()
+    input, logic, artist = initialize.mvc(screen, clock)
 
-    py.init()
-    py.display.set_caption(CAPTION)
-    py.key.set_repeat(KEYDOWN_DELAY)
-    screen = py.display.set_mode(SCREEN_SIZE, flags=py.FULLSCREEN)
-    clock = py.time.Clock()
-
-    options = OptionsHandler()
-    game = GameHandler(options)
-    options.game = game
-    input = InputHandler(game)
-    logic = LogicHandler(game)
-    artist = ArtistHandler(game, screen, clock)
-
-    game.state_handler = game.title
-    game.options.get()
     running = True
+
     while running:
         clock.tick()
         for event in py.event.get():
